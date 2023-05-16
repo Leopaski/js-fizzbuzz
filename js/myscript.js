1ae02450e8c76af2c2cdc009435906483fde58a9
Leopaski <leonardo.pascale@gmail.com>
2023-05-16 17:43:58 +0200

    multipli

--- js/myscript.js
+++ js/myscript.js
@@ -4,3 +4,10 @@ for (let i=1; i<=100; i++){
     const element = i;
     ul.innerHTML += element;
 }
+if (i % 3 ===0){
+    document.getElementById("number").innerHTML="fizz";
+} else if (i % 5 ===0){
+    document.getElementById("number").innerHTML="buzz";
+} else {
+    document.getElementById("number").innerHTML= i;
+}
